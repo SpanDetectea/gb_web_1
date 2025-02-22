6e88b630d3f50df06003813eccf96cc276b012b5
SpanDetectea <spandetectea@gmail.com>
2025-02-22 14:39:16 +0300

    js lasttask

--- index.js
+++ index.js
@@ -35,11 +35,15 @@ const wrapper = document.querySelector('.products__wrapper')
 const data = JSON.parse(dataJson)
 data.forEach(element => {
 
-
     const img = document.createElement('img')
     const title = document.createElement('div')
     const text = document.createElement('div')
     const price = document.createElement('div')
+    const products = document.createElement('div')
+    const addProductButton = document.createElement('button')
+    const buttonImg = document.createElement('img')
+    const ul = document.querySelector('.cart-lists')
+
     img.src = element.img
     img.classList.add('products__wrapper__img')
 
@@ -51,13 +55,32 @@ data.forEach(element => {
     price.textContent = element.price
     price.classList.add('products__wrapper__price')
 
-    const products = document.createElement('div')
     products.classList.add('products__wrapper__items')
+
+    addProductButton.classList.add('products__wrapper__items__button')
+    buttonImg.src = "img/cart.svg"
+    addProductButton.appendChild(buttonImg)
+
+    addProductButton.addEventListener("click", function (e) {
+        const li = document.createElement('li')
+        li.textContent = `Вы добавили ${element.title} за ${element.price} `
+        const deleteEl = document.createElement('span');
+        deleteEl.textContent = 'X'
+        deleteEl.style.marginLeft = "40px"
+        deleteEl.style.color = "red"
+        deleteEl.style.cursor = "pointer"
+        deleteEl.addEventListener('click', function () {
+            ul.removeChild(li)
+        })
+        li.appendChild(deleteEl)
+        ul.appendChild(li)
+    })
 
     products.appendChild(img)
     products.appendChild(title)
     products.appendChild(text)
     products.appendChild(price)
+    products.appendChild(addProductButton)
 
     wrapper.appendChild(products)
 });
